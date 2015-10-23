8fe932ffb1793601a2ba7f1b65f555f8c3e668a6
dfberry <dfberry@dfberrys-MacBook-Air.local>
2015-10-23 13:18:58 -0700

    cleaned up gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -31,20 +31,7 @@ module.exports = function(grunt) {
       mySrcFiles: [jsSrcFiles],
       myTestFiles: [jsTestFiles],
       options: {
-        /*
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        es5: true
-        */
+        node: true
       }     
     }
   });
