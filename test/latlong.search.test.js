70d52b28961794142c8f67fa0e7a9f1ce74972e0
dfberry <dfberry@dfberrys-MacBook-Air.local>
2015-10-23 13:06:14 -0700

    latlong search test done for now

--- test/latlong.search.test.js
+++ test/latlong.search.test.js
@@ -2,29 +2,34 @@
 
 // 3rd party libraries
 var assert = require("chai").assert;
-var path = require("path");
+//var path = require("path");
 
 // custom libraries
 var latlong = require("../latlong.js");
 
 // globals
 var configFile = require('../data/test/config/config1.json');
-var searchTerm	= 'FIELD5';
-var searchValue = 'WA';
 
+describe("Latlong Search", function(){
 
-describe("Latlong Search Test", function(){
-
-	it("via callback", function (done) {
+	it(".ByTerm via callback", function (done) {
 		this.timeout(5000);
+		
+		var searchTerm	= 'FIELD5';
+		var searchValue = 'WA';
+		
 		latlong.Search.ByTerm(configFile, searchTerm, searchValue, function (err, results) {
 			assert.equal(err,null);
 			assert.equal(results.length,747);
 			done();
 		});
 	});
-	it("via promise", function (done) {
+	it(".ByTerm via promise", function (done) {
 		this.timeout(5000);
+		
+		var searchTerm	= 'FIELD5';
+		var searchValue = 'WA';
+
 		latlong.Search.ByTerm(configFile, searchTerm, searchValue)
 		.then(function (results) {
 			assert.equal(results.length,747);
@@ -34,6 +39,21 @@ describe("Latlong Search Test", function(){
 			assert.fail(error);
 			done();
 		});
+	});
+	it(".Fields via callback", function (done) {
+		this.timeout(5000);
+		latlong.Search.Fields(configFile, function (err, results) {
+			assert.equal(err,null);
+			assert.equal(results.length,1);
+			done();
+		});
+	});
+	it(".Fields via promise", function (done) {
+		this.timeout(5000);
+		latlong.Search.Fields(configFile)
+		.then(function (results) {
+			assert.equal(results.length,1);
+			done();
+		});
 	});	
-	
 });	
