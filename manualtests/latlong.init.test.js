8cdcc693fdb6100e0de26b413cef3b488dd1ed13
dfberry <dfberry@dfberrys-MacBook-Air.local>
2015-10-27 09:38:10 -0700

    tweaking status msgs

--- manualtests/latlong.init.test.js
+++ manualtests/latlong.init.test.js
@@ -2,7 +2,7 @@ var latlong = require("../latlong.js");
 
 latlong.Init.Load('../data/test/config/configLoadSuccess.success.json')
 			.then(function (results) {
-				console.log("success:" + JSON.stringify(results));
+				console.log("done:" + JSON.stringify(results));
 			})
 			.fail(function(error){	
 				console.log("error");
